8e2373312211c2dc09ba173413ebe2eed46e0f21
Paweł Otlewski <otlet@otlet.pl>
2017-11-19 12:30:29 +0100

    Fix

--- sortowanie.c
+++ sortowanie.c
@@ -18,14 +18,28 @@ int selectSort(int arr[], int n)
 	return arr;
 }
 
+int minimum (int array[], int size){
+	int min=666;
+	for (int i=0; i<size; i++){
+		if (array[i]<min)
+			min=array[i];
+	}
+	return min;
+}
+
 int main(){
+	
 	int array[5];
 	array[0] = 5;
 	array[1] = 8;
-	array[2] = 0;
+	array[2] = 4;
 	array[3] = 3;
-	array[4] = 2;
-	int elos[5] = selectSort(array, 5);
+	array[4] = 55;
+	
+	printf("%d", minimum(array,5));
+	int elos[5];
+	elos = selectSort(array, 5);
+	
 	for (int i=0; i<5; i++)
 		print("%d\n", elos);
 	return 0;
